1ffe5f1d8f3c3f3f4fc7abc27cc7d6df77bb4bc8
Anastasios Theodorou <32821928+antheodorou@users.noreply.github.com>
2022-01-17 16:49:11 +0200

    Update main.py
    Updated final Main.

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from Model import *
 from Solver import *
 
 m = Model()
-
 m.BuildModel()
-#m.print_nodes()
-#m.print_time_matrix()
 s = Solver(m)
 sol = s.solve()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
